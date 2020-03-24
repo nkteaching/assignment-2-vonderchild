fee01c2675c0f88495d57743124255c5ebb12c96
k180204 Saad Javed <51471592+vonderchild@users.noreply.github.com>
2020-03-25 04:20:12 +0500

    Update question10.cpp

--- chapter5/question10.cpp
+++ chapter5/question10.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 float recursive(int n, int i, float d, float sum) {
     if (i < n) {
-        if (i != 0 && i % 2 == 0) {
+        if (i != 0 && i % 2 == 0) { //if i is not 0 and i is an even number then subtract 1/d from sum
             recursive(n, i + 1, d + 1, sum - (1 / d));
-        } else {
+        } else { //if i is 0 or an odd number add 1/d to sum
             recursive(n, i + 1, d + 1, sum + (1 / d));
         }
-    } else return sum;
+    } else return sum; //return sum
 }
 
 int main() {
