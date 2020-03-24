387f7740d22113d74f143f862dbd6214aac3101f
k180204 Saad Javed <51471592+vonderchild@users.noreply.github.com>
2020-03-25 04:18:27 +0500

    Update question8.cpp

--- chapter5/question8.cpp
+++ chapter5/question8.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-int multiply(int a, int b) {
-    if (a == 0 || b == 0)
+int multiply(int a, int b) { //the function adds a to itself b times
+    if (a == 0 || b == 0) //comparison if a or b is 0
         return 0;
-    return a + multiply(a, b - 1);
+    return a + multiply(a, b - 1); //addition and subtraction
 }
 
 int main() {
