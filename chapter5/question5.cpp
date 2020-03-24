c5c5fefa2a2d4005ef8ef1dc929185f59cf29031
k180204 Saad Javed <51471592+vonderchild@users.noreply.github.com>
2020-03-25 04:07:38 +0500

    Update question5.cpp

--- chapter5/question5.cpp
+++ chapter5/question5.cpp
@@ -2,26 +2,26 @@
 
 using namespace std;
 
-//(a)
-int odd_a(int i, int n) {
+//(a)   //between 1 and n means excluding 1 and n
+int odd_a(int i, int n) { //initially i is 3 and hence the function prints odd numbers starting from 3 to n-1
     if (i >= n)
         return 0;
     else {
         cout << i << " ";
-        return odd_a(i + 2, n);
+        return odd_a(i + 2, n); //i is being incremented by 2
     }
 
 }
 
 //(b)
 int odd_b(int i, int n) {
-    if (n % 2 == 0)
-        return odd_b(i, n - 1);
-    if (n < 3)
+    if (n % 2 == 0) //if n is even
+        return odd_b(i, n - 1); //decrease it by 1 so it becomes odd
+    if (n < 3) //continue till n is less than 3
         return 0;
     else {
         cout << n << " ";
-        return odd_b(i, n - 2);
+        return odd_b(i, n - 2); //n is being decremented by 2
     }
 }
 
