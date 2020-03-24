92665de6bf652856c36c3150af77ceb8a602d032
k180204 Saad Javed <51471592+vonderchild@users.noreply.github.com>
2020-03-25 03:11:48 +0500

    Update question16.cpp

--- chapter5/question16.cpp
+++ chapter5/question16.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//palindrome function remains same for both a and b
+
 int palindrome(string s, int i, int j) {
     if (i == j)
         return 1;
@@ -15,12 +17,12 @@ int palindrome(string s, int i, int j) {
     return 1;
 }
 
-
+//(b) function to remove extra spaces, commas etc
 string replace(string str) {
     string newstring;
     for (char i : str) {
         if (i != ' ' && i != '\'' && i != ',' && i != '.') {
-            newstring += i;
+            newstring += i; //if i is not blank space, comma or an apostrophe then add i to the newstring
         }
     }
 
@@ -35,10 +37,10 @@ int main() {
     else
         cout << "String is not a palindrome" << endl;
 
-
+    //(b)
     string str2{"Madam, I'm Adam"};
-    str2 = replace(str2);
-    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+    str2 = replace(str2); // removes extra characters such as blank spaces, apostrophe, commas etc
+    transform(str2.begin(), str2.end(), str2.begin(), ::tolower); // converts string to lower case
 
     if (palindrome(str2, 0, str2.size()))
         cout << "String is a palindrome" << endl;
