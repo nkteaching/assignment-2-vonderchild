81a2910eb0c48f99fc30c426fd16b51030b89e8e
k180204 Saad Javed <51471592+vonderchild@users.noreply.github.com>
2020-03-25 04:25:39 +0500

    Update question29.cpp

--- chapter5/question29.cpp
+++ chapter5/question29.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-//considering that the stack doesnt overflow and the function runs correctly this is the shortes recursive function i could think of that uses only one variable n
+//considering that the stack doesnt overflow and the function runs correctly this is the shortest recursive function i could think of that uses only one variable n
 int shortest(int n) { // the function iterates n-1 times
     if (n > 0) {
         return shortest(n-1);
